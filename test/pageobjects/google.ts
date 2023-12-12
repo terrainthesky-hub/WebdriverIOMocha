15197820421313e04b453f54f0fcbecc219385b0
Lesley Rich <lesley.t.rich@gmail.com>
2023-12-12 15:46:05 -0500

    made page object and turnary

--- test/pageobjects/google.ts
+++ test/pageobjects/google.ts
@@ -5,6 +5,14 @@ class GooglePages {
     get googleSearchBar(): ChainablePromiseElement {
         return $('//textarea[@aria-label="Search"]')
     }
+    /**
+     * Search text for search results using xpath
+     * @param {string} search
+     * @return {ChainablePromiseElement}
+     */
+    googleSearchResults(search: string): ChainablePromiseElement {
+        return $(`//*[text()="${search}"][1]`)
+    }
 }
 
 export default new GooglePages();
--- test/specs/googleSearch.e2e.ts
+++ test/specs/googleSearch.e2e.ts
@@ -2,15 +2,28 @@ import googlePages from "../pageobjects/google.ts";
 import {constants} from '../data/constants.ts';
 
 describe('I go to google home page and use the search bar', () => {
-    it('I use the search bar', async() => {
-        // const googleURL = 'http://google.com';
+    it('I use the search bar with the word Cats', async() => {
         await browser.url(constants.URLS.google);
         await googlePages.googleSearchBar.setValue('Cats');
         await browser.keys("\uE007");
 
     });
     it('I validate the search results page contains the word Cat', async() => {
-        await $('//*[text()="Cat"][1]').waitForDisplayed();
-        expect(await $('//*[text()="Cat"][1]').getText()).toEqual('Cat');
+        await googlePages.googleSearchResults('Cat').waitForDisplayed();
+        expect(await googlePages.googleSearchResults('Cat').getText()).toEqual('Cat');
+    });
+});
+
+describe('I go to google home page to check for links', () => {
+    it('I iterate through all links and print the text', async() => {
+        await browser.url(constants.URLS.google);
+        const element = await $$('//a');
+        for (let i = 0; i < element.length; i++) {
+            const elementText = await element[i].getText();
+            elementText !== '' && elementText !== undefined
+                ? console.log(`link text:::`, elementText)
+                : console.log(`No text found for element ${i}`);
+        }
+            
     });
 });
